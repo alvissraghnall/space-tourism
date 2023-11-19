3c96d4ed4b37b13d718f27a21caded3b17bb6d1b
Alviss Raghnall <isiomastewart@gmail.com>
2023-11-19 04:40:53 +0100

    basic start

---
+++ src/lib/NavLink.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let active: boolean;
+</script>
+
+<li class="relative h-full font-barlow text-white tracking-wide uppercase text-sm lg:text-base after:absolute after:-bottom-4 after:block after:h-1 after:w-full after:duration-500 after:bg-[#969696] after:scale-0 md:after:-bottom-9 after:opacity-0 { active ? 'after:scale-100 after:bg-white after:opacity-100' : '' }">
+    <slot></slot>
+</li>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+	import "./styles.css";
+	import NavLink from "$lib/NavLink.svelte";
+	import { page } from "$app/stores";
+</script>
+
+<div
+	class="pt-6 lg:pt-10 bg-[url(/background-home-mobile.jpg)] md:bg-[url(/background-home-tablet.jpg)] lg:bg-[url(/background-home-desktop.jpg)]  bg-no-repeat bg-cover min-h-screen h-[44.6875rem] md:h-[64rem] lg:h-[56.25rem] "
+>
+	<nav class="flex items-center justify-between px-5 md:pl-10 md:pr-0 md:h-24 relative">
+		<div><img src="/logo.svg" alt="logo" /></div>
+		<span
+			class=" w-[255px] lg:w-[315px] h-[1px] bg-white absolute top-1/2 left-[10.25rem] z-50 hidden lg:block"
+		/>
+		<div
+			class="hidden md:flex h-full md:items-center justify-between  md:px-11  relative bg-white/5 backdrop-blur-sm"
+		>
+			<ul
+				class="flex items-center lg:justify-center md:space-x-9 lg:space-x-12 w-full lg:pl-[118px] lg:pr-[165px]"
+			>
+				<NavLink active={$page.url.pathname === "/"}><a href="/"><span class="mr-2">00</span>Home</a></NavLink>
+				<NavLink active={$page.url.pathname === "/destination"}><a href="/destination"><span class="mr-2">01</span>Destination</a></NavLink>
+				<NavLink active={$page.url.pathname === "/crew"}><a href="/crew"><span class="mr-2">02</span>crew</a></NavLink>
+				<NavLink active={$page.url.pathname === "/technology"}><a href="/technology"><span class="mr-2">03</span>technology</a></NavLink>
+			</ul>
+		</div>
+		<div class="md:hidden absolute top-5 right-5 z-50 cursor-pointer">
+			<img src="/icon-hamburger.svg" alt="CloseIcon" />
+		</div>
+	</nav>
+	<!--  -->
+	<slot></slot>
+</div>
